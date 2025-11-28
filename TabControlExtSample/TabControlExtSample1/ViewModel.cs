87df643ea3a3104f6851895238d194c49ef71ee7
Karthick-Arjunan <karthick.arjunan@syncfusion.com>
2025-11-28 17:44:03 +0530

    993580-Updated the README.md file and add sample.

---
+++ TabControlExtSample/TabControlExtSample1/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TabControlExtSample1
+{
+	public  class ViewModel : NotificationObject
+	{
+		
+		public ViewModel()
+		{
+
+		}
+
+		public ImageSource DisplayedImage
+		{
+			
+			get {				
+				return new BitmapImage(new Uri("pack://application:,,,/TabControlExtSample1;component/star1.png"));
+				}		
+	
+			
+		}
+
+	}
+}
